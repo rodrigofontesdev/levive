b758293f9342556c6feda3c51aefbf53c9c4efa5
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-12-16 20:19:39 -0300

    build(npm): upgrade Next.js and other dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['eslint:recommended', 'next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      camelcase: 'error',
+      'capitalized-comments': 'warn',
+      'default-case': 'error',
+      'default-case-last': 'error',
+      'default-param-last': 'warn',
+      'dot-notation': 'error',
+      eqeqeq: 'error',
+      'max-classes-per-file': ['error', 1],
+      'new-cap': 'error',
+      'no-duplicate-imports': [
+        'error',
+        {
+          includeExports: true,
+        },
+      ],
+      'no-else-return': 'error',
+      'no-empty-function': 'warn',
+      'no-eq-null': 'error',
+      'no-lonely-if': 'error',
+      'no-throw-literal': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'warn',
+      'require-await': 'error',
+      'sort-vars': 'warn',
+      yoda: 'warn',
+    },
+  }),
+]
+
+export default eslintConfig
