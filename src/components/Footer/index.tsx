7587c6ce312f200bca2ca22090777e41b6e19082
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-12-16 21:51:24 -0300

    feat(component): add floating WhatsApp button on the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,6 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 import logoImage from '@/assets/logo.svg'
+import whatsAppImage from '@/assets/whatsapp-glyph.svg'
+
+const message =
+  'Olá! 😊 Tenho muito interesse em conhecer a clínica e gostaria de tirar algumas dúvidas antes de agendar uma consulta.'
+const clickToChatUrl = `https://api.whatsapp.com/send?phone=5594991331618&text=${encodeURIComponent(message)}`
 
 export function Footer() {
   return (
@@ -75,6 +80,27 @@ export function Footer() {
 
         <div className="text-white text-center">
           &copy; {`${new Date().getFullYear()} Clínica Levive. Todos os direitos reservados.`}
+        </div>
+      </div>
+
+      <div className="fixed right-5 bottom-5 z-50">
+        <div className="w-20 h-20 relative rounded-full group">
+          <div className="animate-ping group-hover:animate-none absolute top-3 left-3 w-14 h-14 rounded-full bg-[#25D366] opacity-75" />
+          <div className="flex justify-center items-center relative rounded-full w-20 h-20 bg-[#25D366] group-hover:brightness-90 transition-all duration-300 ease-in">
+            <Link
+              href={clickToChatUrl}
+              target="_blank"
+              role="button"
+              className="p-4"
+            >
+              <Image
+                src={whatsAppImage}
+                alt=""
+                width={40}
+                height={40}
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
