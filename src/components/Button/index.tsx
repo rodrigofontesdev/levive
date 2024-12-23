74a380667b1d744dc9542c39a5c762eacd11c61c
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-12-22 22:57:58 -0300

    feat(component): create size variant for button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,17 +4,25 @@ import { twMerge } from 'tailwind-merge'
 type ButtonProps = {
   children: ReactNode
   appearance?: 'primary' | 'secondary'
+  size?: 'normal' | 'small'
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button({ children, appearance = 'primary', ...props }: ButtonProps) {
+export function Button({
+  children,
+  appearance = 'primary',
+  size = 'normal',
+  ...props
+}: ButtonProps) {
   const appearanceClass =
-    appearance === 'primary' ? 'bg-black hover:bg-brown' : 'bg-brown hover:bg-dark-brown'
+    appearance === 'secondary' ? 'bg-brown hover:bg-dark-brown' : 'bg-black hover:bg-brown'
+  const sizeClass = size === 'small' ? 'text-sm py-2 px-4' : 'text-lg py-3 px-6'
 
   return (
     <button
       className={twMerge(
+        'inline-block uppercase text-center text-muted transition-colors duration-300 ease-in',
         appearanceClass,
-        'inline-block uppercase text-lg text-center text-muted py-3 px-6 transition-colors duration-300 ease-in',
+        sizeClass,
       )}
       {...props}
     >
