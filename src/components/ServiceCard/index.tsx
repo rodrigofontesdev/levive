c7f3a136b027169c4a3ae2e169d8d3f13da4d04f
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-12-27 00:02:41 -0300

    refactor(component): disable animation in mobile screens for the ServiceCard component

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -2,6 +2,7 @@
 
 import Image, { StaticImageData } from 'next/image'
 import { motion } from 'motion/react'
+import { useEffect, useState } from 'react'
 
 type ServiceCardProps = {
   cover: StaticImageData
@@ -10,6 +11,24 @@ type ServiceCardProps = {
 }
 
 export function ServiceCard({ cover, title, description }: ServiceCardProps) {
+  const [device, setDevice] = useState<'mobile' | 'desktop'>('mobile')
+
+  function changeDevice() {
+    if (window.matchMedia('(min-width: 1024px)').matches) {
+      setDevice('desktop')
+    } else {
+      setDevice('mobile')
+    }
+  }
+
+  useEffect(() => {
+    changeDevice()
+
+    window.addEventListener('resize', changeDevice)
+
+    return () => window.removeEventListener('resize', changeDevice)
+  }, [])
+
   return (
     <article className="w-full h-full relative overflow-hidden">
       <motion.div
@@ -30,27 +49,39 @@ export function ServiceCard({ cover, title, description }: ServiceCardProps) {
 
       <motion.div
         whileHover="hovered"
-        whileTap="tapped"
-        className="absolute inset-0 z-10 flex flex-col justify-end items-center gap-12 p-10 pb-5"
+        className="absolute inset-0 z-10 flex flex-col justify-end items-center gap-8 lg:gap-12 p-5 sm:p-10 sm:pb-5"
       >
-        <motion.div
-          initial={{ y: 0 }}
-          variants={{ hovered: { y: -50 }, tapped: { y: -50, opacity: 1 } }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
-        >
-          <h3 className="uppercase text-4xl md:text-5xl text-muted text-center -tracking-wider">
-            {title}
-          </h3>
-        </motion.div>
+        {device === 'desktop' ? (
+          <>
+            <motion.div
+              initial={{ y: 0 }}
+              variants={{ hovered: { y: -50 } }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+              <h3 className="uppercase text-4xl md:text-5xl text-muted text-center -tracking-wider">
+                {title}
+              </h3>
+            </motion.div>
 
-        <motion.div
-          initial={{ y: 0, opacity: 0 }}
-          variants={{ hovered: { y: -50, opacity: 1 }, tapped: { y: -50, opacity: 1 } }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
-          className="min-h-14"
-        >
-          <p className="lg:text-xl text-muted text-center font-light">{description}</p>
-        </motion.div>
+            <motion.div
+              initial={{ y: 0, opacity: 0 }}
+              variants={{ hovered: { y: -50, opacity: 1 } }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+              <p className="min-h-14 lg:text-xl text-muted text-center font-light">{description}</p>
+            </motion.div>
+          </>
+        ) : (
+          <>
+            <h3 className="uppercase text-4xl md:text-5xl text-muted text-center -tracking-wider -translate-y-[50px]">
+              {title}
+            </h3>
+
+            <p className="min-h-14 lg:text-xl text-muted text-center font-light -translate-y-[50px]">
+              {description}
+            </p>
+          </>
+        )}
       </motion.div>
 
       <div className="absolute inset-0 bg-gradient-to-b from-neutral-900/15 to-neutral-900" />
